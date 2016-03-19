4ce0a2b5c392f3d405dd8570c15ff272db013232
Thom O'Connor <thom.oconnor@yahoo.com>
2016-03-19 14:19:52 -0600

    Fix tripwire compile errors per this discussion: https://sourceforge.net/p/tripwire/bugs/54/

--- src/cryptlib/algebra.h
+++ src/cryptlib/algebra.h
@@ -273,7 +273,7 @@ template <class T> T AbstractEuclideanDomain<T>::Gcd(const Element &a, const Ele
 	Element g[3]={b, a};
 	unsigned int i0=0, i1=1, i2=2;
 
-	while (!Equal(g[i1], this->Zero()))
+	while (!this->Equal(g[i1], this->Zero()))
 	{
 		g[i2] = Mod(g[i0], g[i1]);
 		unsigned int t = i0; i0 = i1; i1 = i2; i2 = t;
