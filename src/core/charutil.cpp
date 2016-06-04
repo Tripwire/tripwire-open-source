acddeb3ae2784c17ced6565fa74b1207a8f67e67
Brian Cox <BrianRCox@gmail.com>
2016-06-04 00:15:29 -0700

    Don't use broken mblen() on AROS

--- src/core/charutil.cpp
+++ src/core/charutil.cpp
@@ -83,12 +83,25 @@ bool cCharUtil::PeekNextChar(  const TSTRING::const_iterator& cur,
     }
 
     first = cur;
+
+    if (!(*cur))
+    {
+       last = cur;
+    }   
+    else
+    {
+#ifndef __AROS__
     mblen (NULL, 0);
     int len = mblen(&*cur, MB_CUR_MAX);
     if (len < 0) //invalid multibyte sequence, but let's not blow up.
         len = 1;
     
     last = cur + len;
+#else  // AROS mblen() seems broken (as of 6/2016) so don't use it.
+    last = cur + 1;
+#endif
+    }
+
     return true;
 }
 
